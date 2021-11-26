d3c521b2e6d215bd00100f20f78af22fc34834c6
Samruddhi Kalsekar <samruddhi@Samruddhis-MacBook-Pro.local>
2021-11-26 14:27:35 -0600

    Adding Queries

---
+++ Source Code/Client/src/components/budget_online.component.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import QueriesDataService from "../services/queries.service";
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.setBudgetOnline = this.setBudgetOnline.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.state = {
+      bonlines : [],
+      currentBOnline: null,
+      currentIndex: -1
+    };
+    
+  }
+
+  componentDidMount() {
+    if(!sessionStorage.getItem("login")) {
+      window.location = "/login"
+    }
+    this.retrieveBOnline();
+  }
+  setBudgetOnline(campaign, index) {
+    this.setState({
+      currentBOnline: campaign,
+      currentIndex: index
+    });
+  }
+
+  retrieveBOnline() {
+    QueriesDataService.budget_online()
+      .then(response => {
+        this.setState({
+          bonlines: response.data.result
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveBOnline();
+    this.setState({
+      currentBOnline: null,
+      currentIndex: -1
+    });
+  }
+
+  render() {
+    const { bonlines, currentBOnline, currentIndex } = this.state;
+
+    return (
+        <div className="list row">
+        <div className="col-md-6">
+        <div>{bonlines.length == 0 &&
+          <h4>No online budget</h4>
+            }
+          </div>
+          <div>{bonlines.length > 0 &&
+          <h4>List</h4>
+            }
+          </div>
+          <ul className="list-group">
+            {bonlines &&
+              bonlines.map((campaign, index) => (
+                <li
+                  className={
+                    "list-group-item " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setBudgetOnline(campaign, index)}
+                  key={index}
+                >
+                  {campaign.campaign_id}
+                </li>
+              ))}
+          </ul>
+          
+        </div>
+
+      
+        <div className="col-md-6">
+          {currentBOnline ? (
+            <div>
+              <h4>Budget of Online Campaign</h4>
+              <div>
+                <label>
+                  <strong>ID :</strong>
+                </label>{" "}
+                {currentBOnline.campaign_id}
+
+              </div>
+              <div>
+                <label>
+                  <strong>Online Campaign Budget</strong>
+                </label>{" "}
+                {currentBOnline.campaign_budget}
+              </div>
+
+            </div>
+      
+
+       
+            
+          ) : (
+            <div>
+              <br />
+              <p>Please click campaign ID </p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+    }
+}
---
+++ Source Code/Client/src/components/designation_pr.component.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import QueriesDataService from "../services/queries.service";
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.setDesignationPR = this.setDesignationPR.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.state = {
+      desginations_pr: [],
+      currentdesginationpr: [],
+      currentIndex: -1
+    };
+    
+  }
+
+  componentDidMount() {
+    if(!sessionStorage.getItem("login")) {
+      window.location = "/login"
+    }
+    this.retrieveDesginationPR();
+  }
+  setDesignationPR(employee, index) {
+
+    this.setState({
+     currentdesginationpr: employee,
+      currentIndex: index
+    });
+    console.log(this.state);
+  }
+
+  retrieveDesginationPR() {
+    QueriesDataService.desgination_pr()
+      .then(response => {
+        this.setState({
+          desginations_pr: response.data.result
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveDesginationPR();
+    this.setState({
+      currentDesginationPR: null,
+      currentIndex: -1
+    });
+  }
+
+  render() {
+    const { desginations_pr, currentdesginationpr, currentIndex } = this.state;
+    console.log(currentdesginationpr)
+    return (
+        <div className="list row">
+        <div className="col-md-6">
+        <div>{desginations_pr.length == 0 &&
+          <h4>No employee designations</h4>
+            }
+          </div>
+          <div>{desginations_pr.length > 0 &&
+          <h4>List</h4>
+            }
+          </div>
+          <ul className="list-group">
+            {desginations_pr &&
+              desginations_pr.map((employee, index) => (
+                <li
+                  className={
+                    "list-group-item " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setDesignationPR(employee, index)}
+                  key={index}
+                >
+                  {employee.employee_id}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className="col-md-6">
+          {currentIndex>=0 ? (
+            <div>
+              <h4>Designation</h4>
+              <div>
+                <label>
+                  <strong>ID :</strong>
+                </label>{" "}
+                {currentdesginationpr.employee_id}
+              </div>
+              <div>
+                <label>
+                  <strong>Designation:</strong>
+                </label>{" "}
+                {currentdesginationpr.employee_designation}
+              </div>
+
+            </div>
+            
+          ) : (
+            <div>
+              <br />
+              <p>Please click on Employee ID</p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+    }
+}
